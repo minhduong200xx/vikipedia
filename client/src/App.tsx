6ecdc816e4fec99ffa61e20279ae2eb4a5bec6a5
minhduong200xx <20010948@st.phenikaa-uni.edu.vn>
2023-10-18 16:00:07 +0700

    login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return ( <BrowserRouter>
-  <Header />
-   <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/Login" element={<Login/>} />
-   </Routes>
-   <Footer />
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<></>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,88 +1,63 @@
-import React from 'react'
-import Pen from '../assets/icons/Pen'
-import Contents from '../assets/icons/Contents'
-import Users from '../assets/icons/users'
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
-
-function Login() {
-    function onChange() {
-      }
+import { useState } from "react";
+const Login = () => {
+  const [human, setHuman] = useState(false);
+  const handleSubmit = (values: unknown) => {
+    console.log(values);
+  };
+  const checkToken = (token: string | null) => {
+    token ? setHuman(true) : setHuman(false);
+  };
   return (
-
-    <div className='mx-6 mt-8'>
-        <div>
-            <p className='font-semibold text-3xl mb-2'>Tạo tài khoản</p>
-            <hr className='' />
-            <p className='my-3 text-sm px-1'>Ngôn ngữ: <span className='cursor-pointer'>
-            Deutsch | English | Esperanto | Français | Español | Italiano | Nederlands | Tiếng Việt
-            </span> </p>
-            <hr />
-            <div className=' w-3/5 h-[600px] border my-4 flex'>
-                <div className='bg-gray-400 w-1/2'>
-                    <div className='mx-4 my-8'>
-                        <h3 class="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight mb-12">
-                            Tạo tài khoản <span class="text-white">Wikipedia</span>
-                        </h3>
-                        <p className='mb-12 text-lg'>Wikipedia là một bách khoa toàn thư mở trực tuyến đa ngôn ngữ được sáng lập và duy trì bởi một cộng đồng biên tập viên tình nguyện.</p>
-                        {/* <p className='font-semibold text-base'>Wikipedia được tạo lên bởi những người như bạn</p> */}
-                        <div className='flex items-center mb-3'>
-                            <div><Pen/></div>
-                            <div className='ml-6 text-lg'>
-                                <span>70.722.918</span>
-                                <p>Lần sửa đổi</p>
-                            </div>                           
-                        </div>
-                        <div className='flex items-center mb-3'>
-                            <div><Contents/></div>
-                            <div className='ml-6 text-lg'>
-                                <span>1.288.245</span>
-                                <p>Bài viết</p>
-                            </div>                           
-                        </div>
-                        <div className='flex items-center'>
-                            <div><Users/></div>
-                            <div className='ml-6 text-lg'>
-                                <span>1.883</span>
-                                <p>Người đóng góp gần đây</p>
-                            </div>                           
-                        </div>
-                    </div>
-                </div>
-                <form className='w-1/2 my-8 mx-6' action="">
-                    <div className='flex flex-col'>
-                        <div className='flex flex-col'>
-                            <label htmlFor="" className='mb-2 text-lg'>Tên đăng nhập</label>
-                            <input required type="text" className=' block w-full text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-lg' placeholder='Nhập tên đăng nhập' />
-                        </div>
-                        <div className='flex w-full mt-6'>
-                            <div className=' flex flex-col w-1/2 mr-6'>
-                                <label className='mb-2' htmlFor="">Mật khẩu</label>
-                                <input required className='block w-full text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-lg' type="password"  placeholder='Nhập mật khẩu' />
-                            </div>
-                            <div className='flex flex-col w-1/2'>
-                                <label className='mb-2' htmlFor="">Nhập lại mật khẩu</label>
-                                <input required className='block w-full text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-lg' type="password" name="" id="" placeholder='Nhập lại mật khẩu' />
-                            </div>
-                        </div>
-                        <div className='my-6'>
-                            <label htmlFor="">Email</label>
-                            <input required placeholder='Nhập vào Email' className='mt-2 block w-full text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-lg' type="text" />
-                        </div>
-                        <ReCAPTCHA 
-                            className=''
-                            sitekey="6LdVipMoAAAAAKI2EvA4ks0wZsmOa5rkeba3XlI8"
-                            onChange={onChange}
-                        />
-                        <button type='submit' className='bg-sky-500 mt-12 h-12 rounded-lg text-white'>
-                            Tạo tài khoản
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <hr className='my-6' />
+    <div className="mx-auto w-96 h-[500px]">
+      <h2 className="font-bold text-xl p-3">Đăng nhập</h2>
+      <Form
+        name="login"
+        className="bg-slate-100 p-10 rounded-lg flex flex-col gap-2"
+        onFinish={handleSubmit}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Tên đăng nhập"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Mật khẩu"
+          />
+        </Form.Item>
+        <ReCAPTCHA
+          className=""
+          sitekey="6LdVipMoAAAAAKI2EvA4ks0wZsmOa5rkeba3XlI8"
+          onChange={checkToken}
+        />
+        <Form.Item>
+          <Button
+            htmlType="submit"
+            disabled={!human}
+            className="shadow-md mx-2 bg-slate-100"
+          >
+            Đăng nhập
+          </Button>
+          Hoặc{" "}
+          <Link to={"/register"} className="font-bold">
+            Đăng ký!
+          </Link>
+        </Form.Item>
+      </Form>
     </div>
-  )
-}
-
-export default Login
+  );
+};
+export default Login;
